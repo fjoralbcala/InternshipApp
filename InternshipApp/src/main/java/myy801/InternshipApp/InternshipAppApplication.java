b7d324c66b689cff3d393f83a281ea121b0d2081
fjoralbcala <fjoralbcala@gmail.com>
2025-03-16 14:36:57 +0200

    Add files via upload

---
+++ InternshipApp/src/main/java/myy801/InternshipApp/InternshipAppApplication.java
@@ -0,0 +1,13 @@
+package myy801.InternshipApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipAppApplication.class, args);
+	}
+
+}
